468f914cf8f1b1ad306d5f8d4b1f66588741b688
yarinapoian <poianyarina@gmail.com>
2024-12-03 14:30:38 +0100

    lab1 version2

--- lab1/task2.js
+++ lab1/task2.js
@@ -1,3 +1,4 @@
+let abcd = [4, true, "hello", -0.333333333, false, 666, "$$$??!@", undefined, {name: "Yarina", age: 17}, 3.14, -2000]
 object = {}
 for (const el of abcd) {
     let elType = typeof el;
